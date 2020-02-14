730fdb5e1eafcf4e8b1806cee7b3d6513df6bcbd
Whitney <ruorohw@gmail.com>
2020-02-15 00:09:35 +0300

    Fix typo

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development" : {
     "username" : process.env.DB_USERNAME,
-    "password" : process.env.DB_PASSOWRD,
+    "password" : process.env.DB_PASSWORD,
     "database" : process.env.DB_NAME,
     "host" : "127.0.0.1",
     "dialect" : "postgres"
