0e34efd50388eec8d796c3b4d363d02fcd669b36
Whitney <ruorohw@gmail.com>
2020-02-21 21:51:43 +0300

    Add invoice associations

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -1,10 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const invoice = sequelize.define('invoice', {
-    transaction_date: DataTypes.DATE
+    transaction_date: DataTypes.DATE,
+    userId: DataTypes.INTEGER
   }, {});
   invoice.associate = function(models) {
-    // associations can be defined here
+    invoice.belongsTo(models.user, {
+      foreignKey: {
+        name : 'userId',
+        type: DataTypes.INTEGER
+      }
+    });
+    invoice.hasMany(models.product_invoice, {
+      foreignKey: 'invoiceNumber'
+    });
   };
   return invoice;
 };
